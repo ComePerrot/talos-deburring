95682351bc051444b78b6f529ce05d04ae5fc4ad
LBMWiksy <delgorguealexis@gmail.com>
2023-07-12 17:02:19 +0200

    cleaning repo

--- pfCalcul/schedule.sh
+++ pfCalcul/schedule.sh
@@ -3,7 +3,7 @@
 #SBATCH --job-name=talosRL
 #SBATCH --ntasks=1
 #SBATCH --array=0-10
-#SBATCH --time=30:00                    ### Maximum Requested time
+#SBATCH --time=10-10:00:00                    ### Maximum Requested time
 #SBATCH --begin=now                     ### Beginning ASAP
 #SBATCH --mail-type=ALL                 ### Send email for (BEGIN, END, FAIL, INVALID_DEPEND, REQUEUE, STAGE_OUT)
 #SBATCH --output=output/output-%A-%a-%N.log    ### Standard outpout STDOUT
@@ -12,6 +12,6 @@
 # Sleep time depends on task ID to prevent all tasks from starting simultaneously
 sleep ${SLURM_ARRAY_TASK_ID} && \
     apptainer run --no-home \
-    --bind /pfcalcul/work/cperrot/logs:/logs \
-    --bind /pfcalcul/work/cperrot/config:/config \
+    --bind /pfcalcul/work/$USER/logs:/logs \
+    --bind /pfcalcul/work/$USER/config:/config \
     rl.sif
