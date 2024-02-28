7a81a1bf76a8a8371ba5ef1f6d3f45b75ccb6182
Côme Perrot <cperrot@laas.fr>
2024-02-28 17:35:45 +0100

    Update parameter loading for more flexibility

--- controllers/MPC.py
+++ controllers/MPC.py
@@ -3,6 +3,7 @@ import pinocchio as pin
 
 from deburring_mpc import OCP
 
+
 class MPController:
     def __init__(self, pinWrapper, x_initial, target_pos, param_ocp):
         self.num_control_knots = 10
@@ -10,10 +11,8 @@ class MPController:
         self.pinWrapper = pinWrapper
         self.param_ocp = param_ocp
 
-        self.param_ocp["state_weights"] = np.array(self.param_ocp["state_weights"])
-        self.param_ocp["control_weights"] = np.array(self.param_ocp["control_weights"])
+        self._get_joint_parameters()
 
-        
         self.oMtarget = pin.SE3.Identity()
         for i in range(3):
             self.oMtarget.translation[i] = target_pos[i]
@@ -26,6 +25,48 @@ class MPController:
         self.horizon_length = param_ocp["horizon_length"]
 
         self.x0 = x_initial
+
+    def _get_joint_parameters(self):
+        # Retrieve controlled joint names
+        controlled_joints_names = self.pinWrapper.get_rmodel().names[1:]
+
+        # Check if robot has a free-flyer
+        free_flyer = "root_joint" in controlled_joints_names
+
+        joint_position_weights = [
+            self.param_ocp["joints"][joint][0]
+            for joint in controlled_joints_names[1 * free_flyer :]
+        ]
+        joint_velocity_weights = [
+            self.param_ocp["joints"][joint][1]
+            for joint in controlled_joints_names[1 * free_flyer :]
+        ]
+
+        if free_flyer:
+            self.param_ocp["state_weights"] = np.concatenate(
+                [
+                    self.param_ocp["base"]["position"],
+                    self.param_ocp["base"]["orientation"],
+                    joint_position_weights,
+                    self.param_ocp["base"]["linear_velocity"],
+                    self.param_ocp["base"]["angular_velocity"],
+                    joint_velocity_weights,
+                ]
+            )
+        else:
+            self.param_ocp["state_weights"] = np.concatenate(
+                [
+                    joint_position_weights,
+                    joint_velocity_weights,
+                ]
+            )
+
+        self.param_ocp["control_weights"] = np.array(
+            [
+                self.param_ocp["joints"][joint][2]
+                for joint in controlled_joints_names[1:]
+            ]
+        )
 
     def change_target(self, x_initial, target_position):
         for i in range(3):
