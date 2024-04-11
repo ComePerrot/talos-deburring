5a2af3fe3f3308b5260ed9f110683ba684162c01
Côme Perrot <cperrot@laas.fr>
2024-04-11 15:50:28 +0200

    Add config to deburring_deep_planner package

--- deburring-planner/node/nn_node.py
+++ deburring-planner/node/nn_node.py
@@ -1,25 +1,33 @@
 #!/usr/bin/env python3
 
-import rospy
-from std_msgs.msg import Float64MultiArray
 import onnxruntime as ort
 import numpy as np
+import rospkg
+import rospy
+
+from std_msgs.msg import Float64MultiArray
+
 
 
 class OnnxNode:
     def __init__(self):
         rospy.init_node("onnx_node")
 
+        # Loading .onnx file path from Ros parameters.
+        rospack = rospkg.RosPack()
+        onnx_path = rospack.get_path("deburring_deep_planner")
+        onnx_file_name = rospy.get_param("nn_file_name")
+        onnx_file = onnx_path + "/config/" + onnx_file_name
+
+        self.ort_sess = ort.InferenceSession(onnx_file)
+
+        rospy.Subscriber("nn_input", Float64MultiArray, self.callback, queue_size=1)
+
         self.observation_size = (231,)
         self.observation = np.zeros((1, *self.observation_size)).astype(np.float32)
         self.pub = rospy.Publisher("scaled_action", Float64MultiArray, queue_size=10)
 
-        self.onnx_path = "test_SAC_model.onnx"
-        self.ort_sess = ort.InferenceSession(self.onnx_path)
-
-        rospy.Subscriber("nn_input", Float64MultiArray, self.callback, queue_size=1)
-
-        self.rate = rospy.Rate(10)  # 10 Hz
+        self.rate = rospy.Rate(20)  # 10 Hz
 
     def callback(self, msg):
         self.observation = (
